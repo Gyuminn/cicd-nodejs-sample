b57ffbdf5a3bce7701ba5d6a3db38ffc3e169a90
Gyuminn <rhkdtlrtm12@hanyang.ac.kr>
2023-12-06 18:58:23 -0600

    change app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { 
-    res.send('Gyumayusi')
+    res.send('I am Gyumayusi')
 });
 
 const port = process.env.PORT || 5000;
